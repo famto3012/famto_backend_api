4e973db355505c550aa6f66a7efda8237029deda
its-sarath28 <sarathrs220@gmail.com>
2024-06-13 10:35:50 +0530

    Added validations

--- routes/categoryRoute/categoryRoute.js
+++ routes/categoryRoute/categoryRoute.js
@@ -6,6 +6,7 @@ const {
 } = require("../../controllers/category/categoryController");
 const { upload } = require("../../utils/imageOperation");
 const isAuthenticated = require("../../middlewares/isAuthenticated");
+const { body } = require("express-validator");
 
 const categoryRoute = express.Router();
 
@@ -13,7 +14,29 @@ const categoryRoute = express.Router();
 categoryRoute.post(
   "/add-category",
   upload.single("categoryImage"),
-  isAuthenticated,
+  [
+    body("bussinessCategoryId")
+      .trim()
+      .notEmpty()
+      .withMessage("Bussiness Category is required"),
+    body("merchantId").trim().notEmpty().withMessage("Merchant is required"),
+    body("categoryName")
+      .trim()
+      .notEmpty()
+      .withMessage("Category name is required"),
+    body("description")
+      .trim()
+      .notEmpty()
+      .withMessage("Description is required"),
+    body("type").trim().notEmpty().withMessage("Type is required"),
+    check("categoryImage").custom((value, { req }) => {
+      if (!req.file || !req.file.categoryImage) {
+        throw new Error("Category image is required");
+      }
+      return true;
+    }),
+  ],
+  // isAuthenticated,
   addCategoryController
 );
 
@@ -21,14 +44,39 @@ categoryRoute.post(
 categoryRoute.put(
   "/edit-category/:categoryId",
   upload.single("categoryImage"),
-  isAuthenticated,
+  [
+    body("bussinessCategoryId")
+      .trim()
+      .notEmpty()
+      .withMessage("Bussiness Category is required"),
+    body("merchantId").trim().notEmpty().withMessage("Merchant is required"),
+    body("categoryName")
+      .trim()
+      .notEmpty()
+      .withMessage("Category name is required"),
+    body("description")
+      .trim()
+      .notEmpty()
+      .withMessage("Description is required"),
+    body("type").trim().notEmpty().withMessage("Type is required"),
+    check("categoryImage").custom((value, { req }) => {
+      if (
+        !req.body.categoryImageURL &&
+        (!req.file || !req.file.categoryImage)
+      ) {
+        throw new Error("Category image is required");
+      }
+      return true;
+    }),
+  ],
+  // isAuthenticated,
   editCategoryController
 );
 
 //TODO: Need to add Role based Authentication
 categoryRoute.delete(
   "/delete-category/:categoryId",
-  isAuthenticated,
+  // isAuthenticated,
   deleteCategoryController
 );
 
