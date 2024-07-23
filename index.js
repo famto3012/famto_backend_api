4c15be19b64f32a924e96e500fa4ad337fac94f7
Akhilbabu456 <akhilbeliever001@gmail.com>
2024-07-23 23:09:17 +0530

    updates

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5175", "*"],
+    origin: ["http://localhost:5173", "*"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
