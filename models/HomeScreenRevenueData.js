3a6318b4d6d8262dcab0aae1e11778a356bf450f
Akhilbabu456 <akhilbeliever001@gmail.com>
2024-10-30 11:58:18 +0530

    updates

--- models/HomeScreenRevenueData.js
+++ models/HomeScreenRevenueData.js
@@ -18,6 +18,10 @@ const HomeScreenRevenueDataSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    order: {
+      type: Number,
+      default: 0,
+    },
     userId: {
       type: String,
       default: null,
