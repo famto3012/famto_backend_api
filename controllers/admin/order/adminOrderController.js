d46c68e58c47e4450885133ca762e6216432fd2f
Sarath R S <78993686+its-sarath28@users.noreply.github.com>
2024-11-01 17:26:14 +0530

    Update adminOrderController.js

--- controllers/admin/order/adminOrderController.js
+++ controllers/admin/order/adminOrderController.js
@@ -231,9 +231,9 @@ const confirmOrderByAdminContrroller = async (req, res, next) => {
     orderFound.status = "On-going";
     orderFound.orderDetailStepper.accepted = stepperData;
 
-    const modelType = orderFound.merchantId.merchantDetail.pricing[0].modelType;
+    const modelType = orderFound?.merchantId?.merchantDetail?.pricing[0]?.modelType;
 
-    if (orderFound.merchantId && modelType === "Commission") {
+    if (orderFound?.merchantId && modelType === "Commission") {
       const { payableAmountToFamto, payableAmountToMerchant } =
         await orderCommissionLogHelper(orderId);
 
@@ -244,9 +244,9 @@ const confirmOrderByAdminContrroller = async (req, res, next) => {
       orderFound.commissionDetail = updatedCommission;
     }
 
-    if(orderFound?.orderDetail?.deliveryMode !== "Take Away"){
+    if (orderFound?.orderDetail?.deliveryMode !== "Take Away") {
       const task = await orderCreateTaskHelper(orderId);
-  
+
       if (!task) {
         return next(appError("Task not created"));
       }
@@ -383,7 +383,7 @@ const rejectOrderByAdminController = async (req, res, next) => {
 
       await orderFound.save();
     } else if (orderFound.paymentMode === "Online-payment") {
-      const paymentId = orderFound.paymentId;
+      const paymentId = orderFound?.paymentId;
 
       let refundResponse;
 
@@ -405,11 +405,13 @@ const rejectOrderByAdminController = async (req, res, next) => {
         updatedTransactionDetail.transactionAmount = refundAmount;
         customerFound.transactionDetail.push(updatedTransactionDetail);
         orderFound.refundId = refundResponse?.refundId;
+
+        await customerFound.save();
       }
+
       updateOrderStatus(orderFound);
 
       await orderFound.save();
-      await customerFound.save();
     }
 
     await ActivityLog.create({
