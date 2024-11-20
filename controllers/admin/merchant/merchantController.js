d99fc49a53ca35dc4238c9f6c74671ee17be7aa9
Akhilbabu456 <akhilbeliever001@gmail.com>
2024-11-20 21:26:20 +0530

    updates

--- controllers/admin/merchant/merchantController.js
+++ controllers/admin/merchant/merchantController.js
@@ -509,7 +509,7 @@ const updateMerchantDetailsByMerchantController = async (req, res, next) => {
           "MerchantLocationImage"
         );
       } catch (err) {
-        res.status(500).json({ error: "Failed to fetch data from Mappls API" });
+        // res.status(500).json({ error: "Failed to fetch data from Mappls API" });
       }
     }
 
@@ -1272,7 +1272,7 @@ const updateMerchantDetailsController = async (req, res, next) => {
   try {
     const { merchantId } = req.params;
 
-    console.log(merchantDetail);
+    // console.log(merchantDetail);
 
     const merchantFound = await Merchant.findById(merchantId);
     if (!merchantFound) {
@@ -1383,7 +1383,7 @@ const updateMerchantDetailsController = async (req, res, next) => {
           "MerchantLocationImage"
         );
       } catch (err) {
-        res.status(500).json({ error: "Failed to fetch data from Mappls API" });
+        // res.status(500).json({ error: "Failed to fetch data from Mappls API" });
       }
     }
 
